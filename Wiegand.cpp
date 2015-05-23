521a7b7651aeb51c821920f89363782f328157e6
Michiel Johan Baird <michiel@asana.com>
2015-05-23 12:32:53 -0400

    Basic Refactor to use bit sizes up to 256
    * Changed to work on variable pins
    * Changed class to not be static
    * Individual state per reader
    
    Needs testing

--- Wiegand.cpp
+++ Wiegand.cpp
@@ -1,18 +1,34 @@
 #include "Wiegand.h"
 
-unsigned long WIEGAND::_cardTempHigh=0;
-unsigned long WIEGAND::_cardTemp=0;
-unsigned long WIEGAND::_lastWiegand=0;
-unsigned long WIEGAND::_sysTick=0;
-unsigned long WIEGAND::_code=0;
-int 		  WIEGAND::_bitCount=0;	
-int			  WIEGAND::_wiegandType=0;
-
 WIEGAND::WIEGAND()
 {
+   init(D0Pin, D1Pin);
 }
 
-unsigned long WIEGAND::getCode()
+WIEGAND::WIEGAND(unsigned int d0Pin, unsigned int d1Pin)
+{
+   init(d0Pin, d1Pin)
+}
+
+void WIEGAND::init(unsigned int d0Pin, unsigned int d1Pin)
+{
+    _tempBuffer = new int[BufferSize];
+    _tempIndexHigh = 0;
+    _tempIndexLow = 0;
+    _lastWiegand=0;
+    _sysTick=0;
+    _code=0;
+    _bitCount=0;
+    _wiegandType=0;
+    _d0Pin = d0Pin;
+    _d1Pin = d1Pin;
+}
+
+void WIEGAND::~WIEGAND() {
+    delete _tempBuffer;
+}
+
+WiegandCode WIEGAND::getCode()
 {
 	return _code;
 }
@@ -30,67 +46,67 @@ bool WIEGAND::available()
 void WIEGAND::begin()
 {
 	_lastWiegand = 0;
-	_cardTempHigh = 0;
-	_cardTemp = 0;
+    _tempIndexHigh = 0;
+    _tempIndexLow = 0;
 	_code = 0;
 	_wiegandType = 0;
 	_bitCount = 0;  
 	_sysTick=millis();
-	pinMode(D0Pin, INPUT);					// Set D0 pin as input
-	pinMode(D1Pin, INPUT);					// Set D1 pin as input
+	pinMode(_d0Pin, INPUT);					// Set D0 pin as input
+	pinMode(_d1Pin, INPUT);					// Set D1 pin as input
 	attachInterrupt(0, ReadD0, FALLING);	// Hardware interrupt - high to low pulse
 	attachInterrupt(1, ReadD1, FALLING);	// Hardware interrupt - high to low pulse
 }
 
 void WIEGAND::ReadD0 ()
 {
-	_bitCount++;				// Increament bit count for Interrupt connected to D0
-	if (_bitCount>31)			// If bit count more than 31, process high bits
-	{
-		_cardTempHigh |= ((0x80000000 & _cardTemp)>>31);	//	shift value to high bits
-		_cardTempHigh <<= 1;
-		_cardTemp <<=1;
-	}
-	else
-	{
-		_cardTemp <<= 1;		// D0 represent binary 0, so just left shift card data
-	}
-	_lastWiegand = _sysTick;	// Keep track of last wiegand bit received
+    readBit(false);
 }
 
 void WIEGAND::ReadD1()
 {
-	_bitCount ++;				// Increment bit count for Interrupt connected to D1
-	if (_bitCount>31)			// If bit count more than 31, process high bits
-	{
-		_cardTempHigh |= ((0x80000000 & _cardTemp)>>31);	// shift value to high bits
-		_cardTempHigh <<= 1;
-		_cardTemp |= 1;
-		_cardTemp <<=1;
-	}
-	else
-	{
-		_cardTemp |= 1;			// D1 represent binary 1, so OR card data with 1 then
-		_cardTemp <<= 1;		// left shift card data
-	}
-	_lastWiegand = _sysTick;	// Keep track of last wiegand bit received
+    readBit(true);
 }
 
-unsigned long WIEGAND::GetCardId (unsigned long *codehigh, unsigned long *codelow, char bitlength)
+void WIEGAND::readBit(bool highBit)
 {
-	unsigned long cardID=0;
+    _bitCount++;
+    _tempBuffer[_tempIndexHigh] &= !(1 << _tempIndexLow);
+    _tempBuffer[_tempIndexHigh] |= highBit << _tempIndexLow;
+    _tempIndexLow++;
+    if (_tempIndexLow == INT_SIZE)
+    {
+        _tempIndexLow = 0;
+        _cardTempHigh++;
+    }
 
-	if (bitlength==26)								// EM tag
-		cardID = (*codelow & 0x1FFFFFE) >>1;
+    _lastWiegand = _sysTick;	// Keep track of last wiegand bit received
+}
 
-	if (bitlength==34)								// Mifare 
+
+WiegandCode WIEGAND::getWiegandCode ()
+{
+	// Bits were read in reverse: So they need to switch back
+
+	WiegandCode code;
+	code.bitCount = _bitCount;
+	bufferIndexH = 0;
+	bufferIndexL = 0;
+	for (int hIndex = _tempIndexHigh; hIndex <= 0; hIndex--)
 	{
-		*codehigh = *codehigh & 0x03;				// only need the 2 LSB of the codehigh
-		*codehigh <<= 30;							// shift 2 LSB to MSB		
-		*codelow >>=1;
-		cardID = *codehigh | *codelow;
+	    for (int lIndex= __tempIndexLow; lIndex <= 0; lIndex--)
+	    {
+	        code.buffer[bufferIndexH] |= ((_tempBuffer[hIndex] >> lIndex) & 1) << bufferIndexL;
+	        bufferIndexL++;
+	        if (bufferIndexL == INT_SIZE)
+	        {
+	            bufferIndexL = 0;
+	            bufferIndexH++;
+	        }
+	    }
+	    _tempIndexLow = INT_SIZE - 1;
 	}
-	return cardID;
+    return code;
 }
 
 bool WIEGAND::DoWiegandConversion ()
@@ -100,58 +116,21 @@ bool WIEGAND::DoWiegandConversion ()
 	_sysTick=millis();
 	if ((_sysTick - _lastWiegand) > 25)								// if no more signal coming through after 25ms
 	{
-		if ((_bitCount==26) || (_bitCount==34) || (_bitCount==8)) 	// bitCount for keypress=8, Wiegand 26=26, Wiegand 34=34
+		if (_bitCount > 0)
 		{
-			_cardTemp >>= 1;			// shift right 1 bit to get back the real value - interrupt done 1 left shift in advance
-			if (_bitCount>32)			// bit count more than 32 bits, shift high bits right to make adjustment
-				_cardTempHigh >>= 1;	
-
-			if((_bitCount==26) || (_bitCount==34))		// wiegand 26 or wiegand 34
-			{
-				cardID = GetCardId (&_cardTempHigh, &_cardTemp, _bitCount);
-				_wiegandType=_bitCount;
-				_bitCount=0;
-				_cardTemp=0;
-				_cardTempHigh=0;
-				_code=cardID;
-				return true;				
-			}
-			else if (_bitCount==8)		// keypress wiegand
-			{
-				// 8-bit Wiegand keyboard data, high nibble is the "NOT" of low nibble
-				// eg if key 1 pressed, data=E1 in binary 11100001 , high nibble=1110 , low nibble = 0001 
-				char highNibble = (_cardTemp & 0xf0) >>4;
-				char lowNibble = (_cardTemp & 0x0f);
-				_wiegandType=_bitCount;					
-				_bitCount=0;
-				_cardTemp=0;
-				_cardTempHigh=0;
-				
-				if (lowNibble == (~highNibble & 0x0f))		// check if low nibble matches the "NOT" of high nibble.
-				{
-					if (lowNibble==0x0b)					// ENT pressed
-					{
-						_code=0x0d;							
-					}
-					else if (lowNibble==0x0a)				// ESC pressed
-					{
-						_code=0x1b;							
-					}
-					else
-					{
-						_code=(int)lowNibble;				// 0 - 9 keys
-					}
-					return true;
-				}
-			}
+		    _code = getWiegandCode();
+            _bitCount=0;
+            _tempIndexHigh = 0;
+            _tempIndexLow = 0;
+            return true;
 		}
 		else
 		{
 			// well time over 25 ms and bitCount !=8 , !=26, !=34 , must be noise or nothing then.
 			_lastWiegand=_sysTick;
-			_bitCount=0;			
-			_cardTemp=0;
-			_cardTempHigh=0;
+            _bitCount=0;
+            _tempIndexHigh = 0;
+            _tempIndexLow = 0;
 			return false;
 		}	
 	}
--- Wiegand.h
+++ Wiegand.h
@@ -9,30 +9,69 @@
 
 #define D0Pin 2			// Arduino Pin 2 Hardware interrupt
 #define D1Pin 3			// Arduino Pin 3 Hardware interrupt
+#define MAX_BIT_LENGTH 256
 
+const int INT_SIZE = sizeof(int)*8;
+const int BUFFER_SIZE = MAX_BIT_LENGTH / INT_SIZE;
+
+struct WiegandCode
+{
+    int bitCount;
+    int* buffer;
+    WiegandCode::WiegandCode()
+    {
+        bitCount = 0;
+        buffer = new int[BUFFER_SIZE];
+        memset(buffer, 0, BUFFER_SIZE);
+    }
+
+    WiegandCode::WiegandCode(const WiegandCode& code)
+    {
+        bitCount = 0;
+        buffer = new int[BUFFER_SIZE];
+        memset(buffer, 0, BUFFER_SIZE);
+        for (int index; index < BUFFER_SIZE; ++index)
+        {
+            buffer[index] = code.buffer[index];
+        }
+    }
+
+    WiegandCode::~WiegandCode
+    {
+        delete buffer;
+    }
+};
 
 class WIEGAND {
 
 public:
 	WIEGAND();
+	~WIEGAND();
+	WIEGAND(unsigned int d0Pin, unsigned int d1Pin);
 	void begin();
 	bool available();
-	unsigned long getCode();
+	WiegandCode getCode();
 	int getWiegandType();
 	
 private:
-	static void ReadD0();
-	static void ReadD1();
-	static bool DoWiegandConversion ();
-	static unsigned long GetCardId (unsigned long *codehigh, unsigned long *codelow, char bitlength);
-	
-	static unsigned long 	_cardTempHigh;
-	static unsigned long 	_cardTemp;
-	static unsigned long 	_lastWiegand;
-	static unsigned long 	_sysTick;
-	static int				_bitCount;	
-	static int				_wiegandType;
-	static unsigned long	_code;
+    void init(unsigned int d0Pin, unsigned int d1Pin)
+	void ReadD0();
+	void ReadD1();
+	void readBit(bool highBit);
+
+	bool DoWiegandConversion ();
+	WiegandCode getWiegandCode();
+
+	unsigned int    _d0Pin;
+    unsigned int    _d1Pin;
+    int*            _tempBuffer;
+    int             _indexHigh;
+    int             _indexLow;
+	unsigned long 	_lastWiegand;
+	unsigned long 	_sysTick;
+	int				_bitCount;
+	int				_wiegandType;
+	WiegandCode     _code;
 };
 
 #endif
